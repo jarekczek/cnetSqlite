7a7884022c038296b04f02ec6471a94c1a74ba33
Jarek Czekalski <jaroslaw.czekalski@bonsoft.pl>
2017-07-08 10:57:54 +0200

    creates a database, counts records

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqllite
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var model = new Model1();
+      Console.WriteLine("rows: " + model.Rows.Count());
+      var r = new Row();
+      r.Name = "hi";
+      model.Rows.Add(r);
+      model.SaveChanges();
+      Console.WriteLine("rows: " + model.Rows.Count());
+    }
+  }
+}
